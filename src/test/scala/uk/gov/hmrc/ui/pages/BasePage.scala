219457d5dc530072a51fecee86e6a179cc2e232e
Susan Swinburne <susan.swinburne@opencastsoftware.com>
2025-10-06 10:00:43 +0100

    VEI-460 ui-test-runner upgrade and waits added

--- src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
+++ src/test/scala/uk/gov/hmrc/ui/pages/BasePage.scala
@@ -16,6 +16,17 @@
 
 package uk.gov.hmrc.ui.pages
 
+import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.{FluentWait, Wait}
 import uk.gov.hmrc.selenium.component.PageObject
+import uk.gov.hmrc.selenium.webdriver.Driver
 
-trait BasePage extends PageObject {}
+import java.time.Duration
+
+trait BasePage extends PageObject {
+
+  def fluentWait: Wait[WebDriver] = new FluentWait[WebDriver](Driver.instance)
+    .withTimeout(Duration.ofSeconds(3))
+    .pollingEvery(Duration.ofMillis(200))
+
+}
